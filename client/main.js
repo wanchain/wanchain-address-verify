e6055ad1a357e6cac82765612e0f6f9318e53260
Harry Ward <harry@wanchain.org>
2018-06-13 09:55:07 -0600

    genesis

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+var Web3 = require('web3');1
+web3 = new Web3();
+
+
+
+
+
